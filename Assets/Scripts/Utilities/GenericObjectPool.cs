e907c73247450e253341d277874e59f1e115c365
abirbhadra <58373108+abirbhadra@users.noreply.github.com>
2025-02-21 01:30:56 +0530

    Implemented Generic Object Pool

---
+++ Assets/Scripts/Utilities/GenericObjectPool.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+
+namespace CosmicCuration.Utilities
+{
+    public class GenericObjectPool<T> where T : class
+    {
+        private List<PooledItem<T>> pooledItems = new List<PooledItem<T>>();
+
+        protected T GetItem()
+        {
+            if (pooledItems.Count > 0)
+            {
+                PooledItem<T> item = pooledItems.Find(item => !item.inUse);
+                if (item != null)
+                {
+                    item.inUse = true;
+                    return item.Item;
+                }
+            }
+            return CreateNewPooledItem();
+        }
+
+        private T CreateNewPooledItem()
+        {
+            PooledItem<T> newItem = new PooledItem<T>();
+            newItem.Item = CreateItem();
+            newItem.inUse = true;
+            pooledItems.Add(newItem);
+            return newItem.Item;
+        }
+
+        protected virtual T CreateItem()
+        {
+            throw new NotImplementedException("Child class don't have Implementation of CreateItem()");
+        }
+
+
+        public class PooledItem<T>
+        {
+            public T Item;
+            public bool inUse;
+        }
+    }
+
+}
